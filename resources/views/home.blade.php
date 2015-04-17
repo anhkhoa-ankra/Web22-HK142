5bbd7895ac3227cf6708dc52976a858df72f6105
Anh Khoa <anhkhoa@nbs.com.vn>
2015-04-17 15:20:20 +0700

    add home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,304 @@
-@extends('app')
+@extends('layouts.master')
+
+@section('title', 'Home')
 
 @section('content')
-<div class="container">
+<div class="page-header">
+	<h1>Business</h1>
+</div>
+<div class="article-title">
+	<div class="row content-news">
+		<div class="col-sm-7">
+			<img src="./images/business_1.jpg">
+		</div>
+		<div class="col-sm-5">
+			<h4>Đà Nẵng tiếp tục dẫn đầu về năng lực cạnh tranh cấp tỉnh</h4>
+			<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+			<span class="sr-only">Time:</span> APRIL 15, 2015
+			<a href="#">
+			<span class="glyphicon glyphicon-thumbs-up like" aria-hidden="true"></span></a>
+			<span class="badge">20</span>
+			<a href="#">
+			<span class="glyphicon glyphicon-comment comment" aria-hidden="true"></span></a>
+			<span class="badge">20</span>
+			<p class="content-news">Xếp hạng năng lực cạnh tranh cấp tỉnh 2014 được Phòng Thương mại &amp; Công nghiệp Việt Nam công bố sáng nay tiếp tục ghi nhận Đà Nẵng ở vị trí quán quân. TP HCM góp mặt trong Top 4.</p>
+			<a href="#" class="read-more"><h4><span class="label label-warning">Read More</span></h4></a>
+		</div>
+	</div>
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
+		<div class="col-sm-6">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_2.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">ZUCKERBERG AND FREE INTERNET IN EUROPE.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> APRIL 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_3.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">GOOGLE ANTITRUST INQUIRIES.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> MAY 05, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_4.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">WEARING VEST MAKE EASY RECRUIT.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> APRIL 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="col-sm-6">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_5.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">BAU DUC DI NUOI BO.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> JULE 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_6.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">EUROPE BANK IS ATTACK.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> MARCH 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_7.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">THE VIETNAM RICHEST WOMAN.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> OCTOBER 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="page-header">
+	<h1>Technology</h1>
+</div>
+<div class="article-title">
+	<div class="row">
+		<div class="col-sm-6">
+			<img src="./images/news_3.jpg">
+			<h4>Google under siege: Europe wants blood</h4>
+			<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+			<span class="sr-only">Time:</span> APRIL 15, 2015
+			<a href="#">
+			<span class="glyphicon glyphicon-thumbs-up like-1" aria-hidden="true"></span></a>
+			<span class="badge">20</span>
+			<a href="#">
+			<span class="glyphicon glyphicon-comment comment" aria-hidden="true"></span></a>
+			<span class="badge">20</span>
+			<p class="content-news">Google faces a fine of up to $6 billion -- more than a quarter's worth of profits for the search company. It also could have to change the way it displays search results in the EU (the changes wouldn't affect results in other countries, including the United States).</p>
+			<a href="#" class="read-more"><h4><span class="label label-warning">Read More</span></h4></a>
+		</div>
+		<div class="col-sm-6">
+			<ul class="list-group">
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_2.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">ZUCKERBERG AND FREE INTERNET IN EUROPE.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> APRIL 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_3.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">GOOGLE ANTITRUST INQUIRIES.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> MAY 05, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_6.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">EUROPE BANK IS ATTACK.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> MARCH 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+				<li class="list-group-item">
+					<div class="row">
+						<div class="col-sm-5"><img src="./images/business_7.jpg"></div>
+						<div class="col-sm-7">
+							<p class="content-news-2">THE VIETNAM RICHEST WOMAN.</p>
+							<p class="content-news-3">
+							<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+							<span class="sr-only">Time:</span> OCTOBER 15, 2015
+							<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+							<span class="sr-only">Author:</span> Yokovu
+							</p>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<div class="page-header">
+	<h1>Stay With Us</h1>
+</div>
+<div class="article-title">
+	<div class="row">
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-facebook.png">
+			<span class="badge">35.500 likes</span>
+		</div>
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-twitter.png">
+			<span class="badge">120 followers</span>
+		</div>
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-youtube.png">
+			<span class="badge">300 subscribers</span>
+		</div>
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-google-plus.png">
+			<span class="badge">450 followers</span>
+		</div>
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-yahoo.png">
+			<span class="badge">190 subscribers</span>
+		</div>
+		<div class="col-sm-2 follower-me">
+			<img src="./images/icon-instagram.png">
+			<span class="badge">560 followers</span>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-sm-6">
+		<div class="page-header">
+			<h1>Sports</h1>
+		</div>
+		<img src="./images/business_8.jpg">
+		<h4>Working for Manchester United's secretive billionaire Glazers</h4>
+		<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+		<span class="sr-only">Time:</span> APRIL 15, 2015
+		<a href="#">
+		<span class="glyphicon glyphicon-thumbs-up like-1" aria-hidden="true"></span></a>
+		<span class="badge">20</span>
+		<a href="#">
+		<span class="glyphicon glyphicon-envelope comment" aria-hidden="true"></span></a>
+		<span class="badge">20</span>
+		<p class="content-news">To hate us when the club has been so successful, it's extraordinary," remarked Joel Glazer after Manchester United's emphatic Champions League victory against AC Milan in March 2010.</p>
+		<a href="#" class="read-more"><h4><span class="label label-warning">Read More</span></h4></a>
+		
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">Six Ways to Grow Your Job</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">How To Deal With A Totally Boring Job</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">How to Ease Business Quarrels and Tension</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">31 Tips for Conquering Startup Fears</a>
+		</div>
+	</div>
+	<div class="col-sm-6">
+		<div class="page-header">
+			<h1>Travelling</h1>
+		</div>
+		<img src="./images/news_5.jpg">
+		<h4>Which Mexican beaches are the safest and best for families?</h4>
+		<span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
+		<span class="sr-only">Time:</span> APRIL 15, 2015
+		<a href="#">
+		<span class="glyphicon glyphicon-thumbs-up like-1" aria-hidden="true"></span></a>
+		<span class="badge">20</span>
+		<a href="#">
+		<span class="glyphicon glyphicon-envelope comment" aria-hidden="true"></span></a>
+		<span class="badge">20</span>
+		<p class="content-news">Are you ready for a vacation at a place that’s beautiful, promises great eats, cultural attractions, fantastic beaches and enough adventures for any adrenaline junkie?  It can be a bargain too.</p>
+		<a href="#" class="read-more"><h4><span class="label label-warning">Read More</span></h4></a>
+		
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">For daring drivers only: The world's scariest roads</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">New travel destination, fees, and one old mistake</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">Most over-the-top experiences at Walt Disney World</a>
+		</div>
+		<div class="page-header title-news-1">
+			<span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+			<a href="#">Larger-than-life hotel pools that make us dropdown-toggle</a>
 		</div>
 	</div>
 </div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>BK Web 22 - @yield('title')</title>
+  <link href="/css/app.css" rel="stylesheet">
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+  <header class="page-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>
+          BKWeb22 <small>Subtext for header</small>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    <nav class="navbar navbar-default">
+      <!-- Brand and toggle get grouped for better mobile display-->
+      <div class="navbar-header">
+        <button type="button" data-toggle="collapse" data-target="#top-navbar-collapse" class="navbar-toggle collapsed">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+        <span class="icon-bar">
+        </span>
+        </button>
+        <a href="/" class="navbar-brand"><span class="glyphicon glyphicon-home"></span></a>
+      </div>
+      <div id="top-navbar-collapse" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li class="active">
+            <a href="#">
+              Link <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">Link</a>
+          </li>
+          <li class="dropdown">
+            <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">
+              Dropdown <span class="caret">
+              </span>
+            </a>
+            <ul role="menu" class="dropdown-menu">
+              <li>
+                <a href="#">Action</a>
+              </li>
+              <li>
+                <a href="#">Another action</a>
+              </li>
+              <li>
+                <a href="#">Something else here</a>
+              </li>
+              <li class="divider">
+              </li>
+              <li>
+                <a href="#">Separated link</a>
+              </li>
+              <li class="divider">
+              </li>
+              <li>
+                <a href="#">One more separated link</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <form role="search" class="navbar-form navbar-right">
+          <div class="input-group">
+            <input type="text" placeholder="Search" class="form-control">
+            <div class="input-group-btn">
+              <button type="submit" class="btn btn-default">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </nav>
+  </div>
+  <header class="jumbotron">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8">
+          <h1>Hello, world!</h1>
+          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <p>
+          <a href="#" role="button" class="btn btn-primary btn-lg">Learn more »</a>
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <div class="row">
+            <div class="col-sm-12">
+              <h2>Hello, world!</h2>
+              <p>
+              <a href="#" role="button" class="btn btn-primary btn-lg">Learn more »</a>
+              </p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12">
+              <h2>Hello, world!</h2>
+              <p>
+              <a href="#" role="button" class="btn btn-primary btn-lg">Learn more »</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <div class="container">
+    <div class="row">
+      <article class="col-sm-8">
+        @yield('content')
+      </article>
+      <aside class="col-sm-4" id="sidebar-main">
+        <div class="row">
+          <div class="col-sm-12">
+            <h4>Social Counter</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <h4>Social Counter</h4>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+            <h4>Custom Menu</h4>
+          </div>
+        </div>
+      </aside>
+    </div>
+  </div>
+  <footer id="footer-large">
+    <div class="container">
+      <div class="col-sm-4">
+        <h4>About Us</h4>
+      </div>
+      <div class="col-sm-4">
+        <h4>Popular Posts</h4>
+      </div>
+      <div class="col-sm-4">
+        <h4>Flickr Photos</h4>
+      </div>
+    </div>
+  </footer>
+  <footer id="footer-lower">
+    <div class="container">
+      <p class="pull-right">
+      <a href="#">Back to top</a>
+      </p>
+      <p>
+      &copy; BKWeb22 2015 · <a href="#">Privacy</a> · <a href="#">Terms</a>
+      </p>
+    </div>
+  </footer>
+  <!-- Scripts -->
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
