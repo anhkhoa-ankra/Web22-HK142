579dcac8bed7f26bd50e18d034e727212a479130
Anh Khoa <anhkhoa@nbs.com.vn>
2015-05-03 07:20:38 +0700

    fix nav-bar error

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,20 +1,24 @@
-$('#nav').affix({
-	offset: {
-		top: $('#nav').offset().top
-	}
-});
-
 $('nav').on('affix.bs.affix', function() {
-	$(this).addClass('navbar-fixed-top');
-	$('>div', this).addClass('container');
+  $(this).addClass('navbar-fixed-top');
+  $('>div', this).addClass('container');
+  $('body').addClass('padding-top-50');
 });
 
 $('nav').on('affix-top.bs.affix', function() {
-	$(this).removeClass('navbar-fixed-top');
-	$('>div', this).removeClass('container');
+  $(this).removeClass('navbar-fixed-top');
+  $('>div', this).removeClass('container');
+  $('body').removeClass('padding-top-50');
+});
+
+$('#nav').affix({
+  offset: {
+    top: $('#nav').offset().top
+  }
 });
 
 $("a[href='#top']").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
+  $("html, body").animate({
+    scrollTop: 0
+  }, "slow");
   return false;
 });
