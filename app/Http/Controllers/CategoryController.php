98d3d18a60f0f7a1fdf478f9e87510602ec56489
Anh Khoa <anhkhoa@nbs.com.vn>
2015-05-03 08:45:44 +0700

    fix authenticated user cannot access home

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -14,7 +14,7 @@ class CategoryController extends Controller {
 	 */
 	public function __construct()
 	{
-		$this->middleware('guest');
+		// $this->middleware('guest');
 		// $this->middleware('auth');
 	}
 
