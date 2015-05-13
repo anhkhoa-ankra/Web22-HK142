9d35011dd43061878354b995749309f520430c29
trannhuvan <tranhuvan@gmail.com>
2015-05-13 21:08:09 +0700

    no message

--- resources/views/layouts/homePostWidget/full.blade.php
+++ resources/views/layouts/homePostWidget/full.blade.php
@@ -1,11 +1,11 @@
 <h4><a href="{{$post->url()}}">{{$post->title}}</a></h4>
 <span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
-<span class="sr-only">Time:</span> APRIL 15, 2015
+<span class="sr-only">Time:</span> {{$post->updated_at}}
 <a href="#">
 <span class="glyphicon glyphicon-thumbs-up like" aria-hidden="true"></span></a>
 <span class="badge">20</span>
 <a href="#">
 <span class="glyphicon glyphicon-comment comment" aria-hidden="true"></span></a>
 <span class="badge">20</span>
-<p class="content-news">Xếp hạng năng lực cạnh tranh cấp tỉnh 2014 được Phòng Thương mại &amp; Công nghiệp Việt Nam công bố sáng nay tiếp tục ghi nhận Đà Nẵng ở vị trí quán quân. TP HCM góp mặt trong Top 4.</p>
+<p class="content-news">{{$post->short_intro}}</p>
 <a href="{{$post->url()}}" class="read-more"><h4><span class="label label-warning">Read More</span></h4></a>
--- resources/views/layouts/homePostWidget/small.blade.php
+++ resources/views/layouts/homePostWidget/small.blade.php
@@ -1,12 +1,17 @@
+
+<?php
+use App\User;
+$user = User::find($post->user_id);
+?>
 <div class="row">
   <div class="col-sm-5 small-featured"><img src="{{$post->featured_img or "/images/no_img.gif"}}"></div>
   <div class="col-sm-7">
     <a class="content-news-2" href="{{$post->url()}}">{{$post->title or "Undefined"}}</a>
     <p class="content-news-3">
     <span class="glyphicon glyphicon glyphicon-time" aria-hidden="true"></span>
-    <span class="sr-only">Time:</span> APRIL 15, 2015
+    <span class="sr-only">Time:</span> {{$post->updated_at}}
     <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-    <span class="sr-only">Author:</span> Yokovu
+    <span class="sr-only">Author:</span> {{$user->name}}
     </p>
   </div>
 </div>
