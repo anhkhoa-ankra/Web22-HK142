c048d0f38a67f8149c35ee4bb410eee1999e0e5c
trannhuvan <tranhuvan@gmail.com>
2015-05-14 17:00:39 +0700

    Edit Catelogy

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,12 +1,25 @@
 @extends('layouts.master')
 @section('title', 'Category')
 @section('content')
+<?php
+use App\Category;
+$cat = Category::find(1);
+$posts = $cat->posts->sortByDesc(function($post) {
+    return $post->posted_at;
+})->take(5);
+
+for ($i=0; $i < count($posts); $i++) { 
+	$datetime[$i] = explode(" ",$posts[$i]->updated_at);
+	$date[$i] = $datetime[$i][0];
+}
+
+?> <!--
 <div class="row">
 	<div class="col-sm-4 part-1">
 		<div class="large">
 			<article id ="post-1" class="post-1" itemtype="http://schema.org/Article" itemscope="itemscope">
 				<a class = "post-url" href="#">
-					<img src="./images/dota2.jpg" alt="Test1" width="360" height="165" class="img-responsive">
+					<img src="./images/catelogy-1.jpg" alt="Test1" width="360" height="165" class="img-responsive">
 				</a>
 				
 				<div class="content">
@@ -21,7 +34,7 @@
 		
 		<article id ="post-2" class="post-2"  itemtype="http://schema.org/Article" itemscope="itemscope">
 			<a class = "post-url" href="#">
-				<img src="./images/dota2.jpg" alt="Test4" width="360" height="165" class="img-responsive">
+				<img src="./images/catelogy-1.jpg" alt="Test4" width="360" height="165" class="img-responsive">
 			</a>
 			
 			<div class="content">
@@ -37,7 +50,7 @@
 		<div class="large">
 			<article itemtype="http://schema.org/Article" itemscope="itemscope">
 				<a class = "post-url" href="#">
-					<img src="./images/dota2.jpg" alt="Test2" width="360" height="165" class="img-responsive">
+					<img src="./images/catelogy-1.jpg" alt="Test2" width="360" height="165" class="img-responsive">
 				</a>
 				
 				<div class="content">
@@ -52,7 +65,7 @@
 		
 		<article itemtype="http://schema.org/Article" itemscope="itemscope">
 			<a class = "post-url" href="#">
-				<img src="./images/dota2.jpg" alt="Test5" width="360" height="165" class="img-responsive">
+				<img src="./images/catelogy-1.jpg" alt="Test5" width="360" height="165" class="img-responsive">
 			</a>
 			
 			<div class="content">
@@ -68,7 +81,7 @@
 		<div class="large">
 			<article itemtype="http://schema.org/Article" itemscope="itemscope">
 				<a class = "post-url" href="#">
-					<img src="./images/dota2.jpg" alt="Test3" width="360" height="165" class="img-responsive">
+					<img src="./images/catelogy-1.jpg" alt="Test3" width="360" height="165" class="img-responsive">
 				</a>
 				
 				<div class="content">
@@ -83,7 +96,7 @@
 		
 		<article itemtype="http://schema.org/Article" itemscope="itemscope">
 			<a class = "post-url" href="#">
-				<img src="./images/dota2.jpg" alt="Test6" width="360" height="165" class="img-responsive">
+				<img src="./images/catelogy-1.jpg" alt="Test6" width="360" height="165" class="img-responsive">
 			</a>
 			
 			<div class="content">
@@ -95,101 +108,58 @@
 			</div>
 		</article>
 	</div>
-</div>
+</div> -->
 <!-- <main id="content" class="container" itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage" itemscope="" role="main"> -->
 <div class="row">
-	<div class="main-content col-sm-12 no-sidebar" itemtype="http://schema.org/Blog" itemscope="itemscope">
-		<h1 class="page-heading">
-		<span class="h-title">
-		<i>Category 1</i>
-		</span>
-		</h1>
-		<article class= "bottom-line blog-block" itemtype="http://schema.org/Article" itemscope="itemscope">
-			<div class="row">
+	<div class="col-sm-12 title-line">
+		<h4 class="page-heading">
+			<span class="h-title">
+				Browsing : <i>{{$cat->name}}</i>
+			</span>
+		</h4>
+		<div class="col-sm-2 line-orrange"></div>
+		<div class="col-sm-10 line-gray"></div>
+	</div>
+	<div class="col-sm-12" itemtype="http://schema.org/Blog" itemscope="itemscope">
+@for ($i = 0; $i < count($posts); $i+=1)
+		<article class= "catelogy-block" itemtype="http://schema.org/Article" itemscope="itemscope">
+			<div class="row catelogy-term">
 				<div class="col-sm-6">
-					<img src="./images/dota2.jpg" width="600" height="300" alt="BAI_1" class="img-responsive">
-					<span class="term-title term-9">
-					<a href="#">Category 1</a>
+					<img class="catelogy-term-img" src="./images/catelogy-1.jpg" alt="BAI_1" class="img-responsive">
+					<span class="catelogy-term-title">
+						<a href="#" class="read-more"><h4><span class="label label-danger">{{$cat->name}}</span></h4></a>
 					</span>
 				</div>
 				<div class="col-sm-6">
-					<h2 class="title">
-					<a class="post-url" title="Post 1" href="#" rel="bookmark" itemprop="url">
-						<span class="post-title" itemprop="headline">Post 1</span>
-					</a>
-					</h2>
-					<div class="post-details">
-						<span class="time">
-						<i class="fa fa-clock-o"></i>
-						<time class="post-published updated" title="Friday, April 16, 2015, 00:00 am" datetime="2015-04-16T00:00:00+00:00">April 16, 2015</time>
-						</span>
-						<a class="comments" itemprop="interactionCount" title="Leave a comment on: “Post 1”" href="#">
-							<i class="fa fa-comments-o"></i>
+					<div class="catelogy-post-detail">
+						<a class="post-url" title="Post 1" href="#" rel="bookmark" itemprop="url">
+							<span class="post-title" style="font-size: 18px;" itemprop="headline">{{$posts[$i]->title}}</span>
 						</a>
-						<div class="fa rating-stars">
-							<span style="width: 66%;"></span>
+						<div class="catelogy-post-time-author row">
+							<div class="col-sm-4">
+								<span><time title="{{$posts[0]->updated_at}}">{{$date[$i]}}</time></span>
+							</div>
+							<div class="col-sm-6">
+								<span><a href="#"><span>{{$posts[$i]->author->name}}</span></a></span>
+								<!-- FOR STAR RATING
+								<div class="rating-stars">
+									<span style="width: 66%;"></span>
+								</div>
+								-->
+							</div>
+							<div class="col-sm-2 title-comment">
+								<a href="#" title="Click here to view commentsâ€">3</a>
+							</div>
 						</div>
-						<span class="author" itemtype="http://schema.org/Person" itemscope="itemscope" itemprop="author">
-						<a class="author-url" href="#" itemprop="url">
-							<span class="author-name" itemprop="givenName">
-							<i class="fa fa-user"></i>
-							John Doe
-							</span>
-						</a>
-						</span>
+						<div class="post-summary" itemprop="description">
+							{{$posts[$i]->short_intro}}
+						</div>
 					</div>
-					<div class="post-summary" itemprop="description">
-						Lorem ipsum quam a platea interdum justo, nostra ligula pretium ultricies euismod aptent, hendrerit velit curae quis risus sagittis a eros tincidunt morbi purus lobortis praesent nibh, consectetur sit pellentesque curabitur senectus litora. Duis ut mollis senectus mollis cursus feugiat duis tortor euismod aenean quisque, lacinia eu orci sodales quisque scelerisque condimentum nibh varius. Accumsan ultrices conubia praesent fringilla nam sed mollis nec potenti volutpat,…
-					</div>
-					<a class="read-more" propname="url" title="Read More" href="#">
-						Read More
-					</a>
+					<a href="#" class="read-more"><h4><span class="label label-danger">READ MORE</span></h4></a>
 				</div>
 			</div>
 		</article>
-		<article class="bottom-line blog-block" itemtype="http://schema.org/Article" itemscope="itemscope">
-			<div class="row">
-				<div class="col-sm-6">
-					<img src="./images/dota2.jpg" width="600" height="300" alt="BAI_1" class="img-responsive">
-					<span class="term-title term-9">
-					<a href="#">Category 1</a>
-					</span>
-				</div>
-				<div class="col-sm-6">
-					<h2 class="title">
-					<a class="post-url" title="Post 2" href="#" rel="bookmark" itemprop="url">
-						<span class="post-title" itemprop="headline">Post 2</span>
-					</a>
-					</h2>
-					<div class="post-details">
-						<span class="time">
-						<i class="fa fa-clock-o"></i>
-						<time class="post-published updated" title="Friday, April 16, 2015, 00:00 am" datetime="2015-04-16T00:00:00+00:00">April 16, 2015</time>
-						</span>
-						<a class="comments" itemprop="interactionCount" title="Leave a comment on: “Post 1”" href="#">
-							<i class="fa fa-comments-o"></i>
-						</a>
-						<div class="fa rating-stars">
-							<span style="width: 66%;"></span>
-						</div>
-						<span class="author" itemtype="http://schema.org/Person" itemscope="itemscope" itemprop="author">
-						<a class="author-url" href="#" itemprop="url">
-							<span class="author-name" itemprop="givenName">
-							<i class="fa fa-user"></i>
-							John Doe
-							</span>
-						</a>
-						</span>
-					</div>
-					<div class="post-summary" itemprop="description">
-						Lorem ipsum proin urna dui phasellus sit donec, consequat commodo tristique feugiat eros eu lorem, primis nullam morbi quis nisi netus tincidunt faucibus arcu et congue lobortis curabitur odio etiam nisl himenaeos est et. Tempus varius semper sagittis imperdiet porta pretium egestas blandit ut imperdiet, fusce adipiscing semper scelerisque in adipiscing arcu fusce tellus etiam, lacinia cras bibendum nisl est vel phasellus ad vehicula. Mattis…
-					</div>
-					<a class="read-more" propname="url" title="Read More" href="#">
-						Read More
-					</a>
-				</div>
-			</div>
-		</article>
+@endfor
 	</div>
 </div>
 <!-- </main> -->
