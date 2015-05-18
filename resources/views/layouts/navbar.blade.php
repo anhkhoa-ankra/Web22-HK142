b77a0d55107e08a9c0a11a426f6a1091543d87ff
trannhuvan <tranhuvan@gmail.com>
2015-05-18 20:57:25 +0700

    edit navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
   <nav id="nav" class="navbar navbar-default">
     <div class="nav-background">
       <!-- Brand and toggle get grouped for better mobile display-->
-      <div class="navbar-header">
+      <div class="navbar-header alpha60">
         <button type="button" data-toggle="collapse" data-target="#top-navbar-collapse" class="navbar-toggle collapsed">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar">
@@ -12,13 +12,17 @@
           <span class="icon-bar">
           </span>
         </button>
-        <a href="/" class="navbar-brand"><span class="nav-font-color"><span class="glyphicon glyphicon-home" style="padding-left:5px; padding-right:5px;"></span></span></a>
+        <a href="/" class="navbar-brand">
+          <span class="nav-font-color">
+            <span class="glyphicon glyphicon-home" style="padding-left:5px; padding-right:5px;"></span>
+          </span>
+        </a>
       </div>
       <div id="top-navbar-collapse" class="collapse navbar-collapse">
         <ul class="nav navbar-nav nav-font-color">
           <li class="active">
             <a href="#">
-              <span class="glyphicon glyphicon-home visible-sm-inline"></span><span class="hidden-sm nav-font-color"> Trang chủ</span><span class="sr-only">(current)</span>
+              <span class="glyphicon glyphicon-home visible-sm-inline"></span><span class="hidden-sm nav-font-color"> TRANG CHỦ</span><span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="alpha60">
@@ -26,7 +30,7 @@
               <span class="nav-font-color"> <span class="glyphicon glyphicon-leaf"></span> GIỚI THIỆU</span>
             </a>
           </li>
-          <li class="dropdown">
+          <li class="dropdown alpha60">
             <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">
               <span class="nav-font-color">
                 <span class="glyphicon glyphicon-list"></span>
@@ -34,66 +38,66 @@
                 <span class="caret"></span>
               </span> 
             </a>
-            <ul role="menu" class="dropdown-menu nav-background">
+            <ul role="menu" class="dropdown-menu nav-background alpha60">
               <li>
-                <a href="#"><span class="nav-font-color">Action</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Action</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Another action</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Another action</span></a>
               </li>              
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Something else here</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Something else here</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Separated link</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Separated link</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">One more separated link</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> One more separated link</span></a>
               </li>
             </ul>
           </li>
-          <li class="dropdown">
-            <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">
+          <li class="dropdown alpha60">
+            <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle alpha60">
               <span class="nav-font-color">
                 <span class="glyphicon glyphicon-star"></span> SỰ KIỆN 
                 <span class="caret"></span> 
               </span>
             </a>
-            <ul role="menu" class="dropdown-menu nav-background">
+            <ul role="menu" class="dropdown-menu nav-background alpha60">
               <li>
-                <a href="#"><span class="nav-font-color">Action</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Action</span></a>
               </li>              
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Another action</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Another action</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Something else here</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Something else here</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">Separated link</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> Separated link</span></a>
               </li>
               <li class="divider">
               </li>
               <li>
-                <a href="#"><span class="nav-font-color">One more separated link</span></a>
+                <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-play" style="font-size:10px; margin-right:5px;"></span> One more separated link</span></a>
               </li>
             </ul>
           </li>
-          <li>
+          <li class="alpha60">
             <a href="#"><span class="nav-font-color"><span class="glyphicon glyphicon-pencil"></span> LIÊN HỆ</span></a>
           </li>
         </ul>
